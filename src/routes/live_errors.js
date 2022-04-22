bd7dc2ec25e6b66b8d050fa2d0f5915dd1d825f3
Alexander Eatman <johnathanae1995@gmail.com>
2022-04-22 11:59:49 -0400

    update

---
+++ src/routes/live_errors.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var errors = require('../../build.js');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { error_set: errors.errors });
+});
+
+module.exports = router;
