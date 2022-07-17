3c0e4fa73043f842f2138a8c6dd425dbd5fdfb65
Alexander Eatman <johnathanae1995@gmail.com>
2022-07-17 12:04:18 -0400

    update

--- example/interval.js
+++ example/interval.js
@@ -3,4 +3,4 @@ live_changes.set_working_set({ index_a: { on: true, paths: ['../example/function
 console.log(live_changes.get_working_set());
 live_changes.start_interval();
 console.log(live_changes.interval_status());
-// console.log(live_changes.stop_interval());
+//live_changes.stop_interval();
